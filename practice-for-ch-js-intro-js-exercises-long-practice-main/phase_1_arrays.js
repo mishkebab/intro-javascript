3f4c7a88c7eb86b20e00f71e29f83a775b0fa032
misha <mishabansal7@gmail.com>
2023-05-16 16:03:29 -0700

    finished phase 1 and 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
@@ -11,6 +11,41 @@ Array.prototype.uniq = function() {
     return result;
 };
 
-arr = [1,2,3,3,3,3]
+// arr = [1,2,3,3,3,3]
+// console.log(arr.uniq())
 
-console.log(arr.uniq())
+Array.prototype.twoSum = function() {
+    let result = [];
+    for (let i = 0; i < this.length; i++) {
+      let num1 = this[i];
+      for (let j = i + 1; j < this.length; j++) {
+        let num2 = this[j];
+        if (num1 + num2 === 0) {
+          result.push([i, j]);
+        }
+      }
+    }
+    return result;
+  }
+
+// arr = [-1, 0, 2, -2, 1];
+// console.log(arr.twoSum());
+
+Array.prototype.transpose = function() {
+    let result = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let subArr = [];
+        for (let j = 0; j < this.length; j++) {
+            subArr.push(this[j][i]);
+        };
+        result.push(subArr);
+    };
+
+    return result;
+}
+
+// arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+// console.log(arr.transpose())
+
+
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_2_enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_2_enumerable.js
@@ -1,1 +1,57 @@
 // Your code here
+
+Array.prototype.myEach = function(callback) {
+    for(i = 0; i < this.length; i ++) {
+        callback(this[i]);
+    }
+} 
+
+// arr = [2,4,6];
+
+// doubler = function(num) {
+//     console.log(num * 2);
+// }
+
+// console.log(arr.myEach(doubler));
+
+Array.prototype.myMap = function(callback) {
+    let result = [];
+
+    this.myEach(ele => {
+        result.push(callback(ele));
+    });
+
+    return result;
+}
+
+add_two = function(num) {
+    return num + 2;
+}
+
+// arr = [2,4,6];
+
+// console.log(arr.myMap(add_two));
+
+Array.prototype.myReduce = function(callback, initialValue) {
+    let arr = this;
+    let acc;
+    if (initialValue !== undefined) {
+        acc = initialValue; 
+    } else {
+        acc = this[0];
+        arr = arr.slice(1)
+    } 
+
+    arr.myEach(el => {
+        acc = callback(acc, el); 
+    });
+
+    return acc;
+}
+
+arr = [1, 2, 3];
+reduced = arr.myReduce(function(acc, el) {
+    return acc + el;
+}, 25);
+
+console.log(reduced);
