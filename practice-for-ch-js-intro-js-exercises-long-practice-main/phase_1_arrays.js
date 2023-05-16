bbd9ca79a8d77b85a9c78a68295c4eba0161af12
misha <mishabansal7@gmail.com>
2023-05-16 13:58:20 -0700

    finished uniq

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
@@ -1,1 +1,16 @@
 // Your code here
+
+
+Array.prototype.uniq = function() {
+    let result = []
+    this.forEach(ele => {
+        if (!result.includes(ele)) {
+            result.push(ele);
+        };
+    })
+    return result;
+};
+
+arr = [1,2,3,3,3,3]
+
+console.log(arr.uniq())
