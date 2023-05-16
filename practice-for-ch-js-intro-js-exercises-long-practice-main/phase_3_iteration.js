2dcd0cd71a13004589ded8de1ee169b0c9483497
misha <mishabansal7@gmail.com>
2023-05-16 16:53:19 -0700

    finished phase 3

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_3_iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_3_iteration.js
@@ -16,6 +16,26 @@ Array.prototype.bubbleSort = function() {
   return this;
 }
 
-arr = [3,2,1];
-console.log(arr.bubbleSort());
+// arr = [3,2,1];
+// console.log(arr.bubbleSort());
+
+String.prototype.substrings = function() {
+    let result = [];
+
+    for (let i = 0; i < this.length; i++) {
+        // let char = this[i];
+        // result.push(char);
+            for (j = i + 1; j <= this.length; j++) {
+                result.push(this.slice(i, j));
+            }
+    }
+
+    return result;
+}
+
+test_string = "string";
+console.log(test_string.substrings());
+
+
+
 
